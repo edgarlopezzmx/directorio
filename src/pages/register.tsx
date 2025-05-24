e9483e79745f3642862b032eab13d3e1b421a8bd
elopezz <edgarlopezz.mx@gmail.com>
2025-05-24 12:34:24 -0600

    instalacion de axios

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -6,6 +6,7 @@ export default function RegisterUser() {
         email: "",
         name: "",
         password: "",
+        confirmPassword: "",
     });
 
     const handleChange = (e) => {
@@ -24,9 +25,9 @@ export default function RegisterUser() {
             const response = await axios.post("/api/users", formData);
             if (response.status === 201) {
                 alert("User registered successfully!");
-                setFormData({ email: "", name: "", password: "" }); // Reset form
+                setFormData({ email: "", name: "", password: "", confirmPassword:"" }); // Reset form
             }
-            const data = await response.data.json();
+            const data = await response.data;
             alert(`User registered successfully: ${data.name}`);
         } catch (error) {
             console.error("Error registering user:", error);
@@ -38,24 +39,55 @@ export default function RegisterUser() {
             <div className="p-6 max-w-md mx-auto">
                 <h1 className="text-xl mb-4">Register</h1>
                 <form onSubmit={handleSubmit} className="space-y-4">
-                    <input
-                    name="name"
-                    type="text"
-                    placeholder="Name"
-                    className="w-full border p-2"
-                    value={formData.name}
-                    onChange={handleChange}
-                    required
-                    />
-                    <input
-                    name="email"
-                    type="email"
-                    placeholder="Email"
-                    className="w-full border p-2"
-                    value={formData.email}
-                    onChange={handleChange}
-                    required
-                    />
+                    <div>
+                        <label htmlFor="name" className="block mb-2">Name</label>
+                        <input
+                        name="name"
+                        type="text"
+                        placeholder="Name"
+                        className="w-full border p-2"
+                        value={formData.name}
+                        onChange={handleChange}
+                        required
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="email" className="block mb-2">Email</label>
+                        <input
+                        name="email"
+                        type="email"
+                        placeholder="Email"
+                        className="w-full border p-2"
+                        value={formData.email}
+                        onChange={handleChange}
+                        required
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="password" className="block mb-2">Password</label>
+                        <input
+                        name="password"
+                        type="password"
+                        placeholder="Password"
+                        className="w-full border p-2"
+                        value={formData.password}
+                        onChange={handleChange}
+                        required
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="confirmPassword" className="block mb-2">Confirm Password</label>
+                        <input
+                        name="confirmPassword"
+                        type="password"
+                        placeholder="Confirm Password"
+                        className="w-full border p-2"
+                        value={formData.confirmPassword}
+                        onChange={handleChange}
+                        required
+                        />
+                    </div>
+
                     <button type="submit" className="bg-blue-500 text-white px-4 py-2">
                     Register
                     </button>
