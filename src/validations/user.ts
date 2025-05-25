ff0e0d9e4e94f8b0c0d11bfbb8e5257b866fcbe8
elopezz <edgarlopezz.mx@gmail.com>
2025-05-25 09:06:27 -0600

    esquema de validacion de datos de entrada

---
+++ src/validations/user.ts
@@ -0,0 +1,15 @@
+import { Message } from './../../node_modules/typescript/lib/typescript.d';
+import { z } from 'zod';
+
+export const userBaseSchema = z.object({
+    name: z.string().min(1, { message: 'El nombre es requerido' }),
+    email: z.string().email({ message: 'Email inválido' }),
+});
+
+export const userRegisterSchema = userBaseSchema.extend({
+    password: z.string().min(6, { message: 'La contraseña debe tener al menos 6 caracteres' }),
+});
+
+export const userUpdateSchema = userBaseSchema.extend({
+    password: z.string().min(6, { message: 'La contraseña debe tener al menos 6 caracteres'}).optional(),
+});
